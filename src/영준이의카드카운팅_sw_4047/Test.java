147c2b42d4841b734c51762a4659bcb80945e1b7
hwlee627 <juicyson627@gmail.com>
2024-01-30 17:42:46 +0900

    영준이의카드카운팅완료

---
+++ src/영준이의카드카운팅_sw_4047/Test.java
@@ -0,0 +1,17 @@
+package 영준이의카드카운팅_sw_4047;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 04;
+		System.out.println(a);
+		
+		String b = "S04";
+		String[] c = String.valueOf(b).split("");
+		
+		int d = Integer.parseInt(c[1]+c[2]);
+		System.out.println(d);
+	}
+
+}
