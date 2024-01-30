7ea41ca0bf969b697d60c7a5e9c4a7ab3898854f
hwlee627 <juicyson627@gmail.com>
2024-01-30 16:50:15 +0900

    테네스의 특별한 소수

---
+++ src/테네스씨의특별한소수_sw_4698/Test.java
@@ -0,0 +1,25 @@
+package 테네스씨의특별한소수_sw_4698;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean[] primeNumber = new boolean [1000001];
+		
+		primeNumber[0] = true;
+		primeNumber[1] = true;
+		
+		for (int i = 2; i < Math.sqrt(1000000); i++) {
+			if(primeNumber[i]) {
+				continue;
+			}
+			for(int j = i*i; j < primeNumber.length; j = j+i) {
+				primeNumber[j] = true;
+			}
+		}
+		System.out.println(Arrays.toString(primeNumber));
+	}
+
+}
