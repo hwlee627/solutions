56060f5c939d47c82921071871b81b4808e5e69a
hwlee627 <juicyson627@gmail.com>
2024-02-05 15:01:53 +0900

    색종이 완료

---
+++ src/색종이_bj_10163_3w/Test.java
@@ -0,0 +1,14 @@
+package 색종이_bj_10163_3w;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "abc";
+		
+		a=a.replace("b", "");
+		
+		System.out.println(a);
+	}
+
+}
