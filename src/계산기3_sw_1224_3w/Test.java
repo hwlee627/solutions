e2079db70dddfb4165680eccf6119c38c3ad1671
hwlee627 <juicyson627@gmail.com>
2024-02-06 14:46:59 +0900

    계산기 완료

---
+++ src/계산기3_sw_1224_3w/Test.java
@@ -0,0 +1,15 @@
+package 계산기3_sw_1224_3w;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = '1';
+		char b = '2';
+		int c = a-'0';
+		System.out.println(c);
+		
+		System.out.println('1+2');
+	}
+
+}
