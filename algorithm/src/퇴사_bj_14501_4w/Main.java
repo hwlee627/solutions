3cac39fbe1160ce6e13bc1bd88d20ce532377b93
hwlee627 <juicyson627@gmail.com>
2024-02-16 16:32:47 +0900

    ACM호텔 완료

--- algorithm/src/퇴사_bj_14501_4w/Main.java
+++ algorithm/src/퇴사_bj_14501_4w/Main.java
@@ -7,9 +7,32 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		long N = sc.nextInt();
-			
-		System.out.println(4*N);
+		int upCnt = 0;
+		int downCnt = 0;
+		int tmp = 0;
+		for(int i = 0; i<8; i++) {
+			int num = sc.nextInt();
+			if(i >= 1) {
+				if(tmp > num) {
+					downCnt++;
+					tmp = num;
+				}if(tmp < num) {
+					upCnt++;
+					tmp = num;
+				}
+			}else {
+				tmp = num;
+				upCnt++;
+				downCnt++;
+			}
+		}
+		if(downCnt == 8) {
+			System.out.println("descending");
+		}else if(upCnt == 8) {
+			System.out.println("ascending");
+		}else {
+			System.out.println("mixed");
+		}
 		
 	}
 }
