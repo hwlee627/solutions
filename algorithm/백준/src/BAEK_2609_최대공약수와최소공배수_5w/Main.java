916a0d802ad3af76f8a1738e4777fc793987edfc
hwlee627 <juicyson627@gmail.com>
2024-02-20 11:22:57 +0900

    최대공약수와최대공배수완료

--- algorithm/백준/src/BAEK_2609_최대공약수와최소공배수_5w/Main.java
+++ algorithm/백준/src/BAEK_2609_최대공약수와최소공배수_5w/Main.java
@@ -1,17 +1,16 @@
-package BAEK_14501_퇴사_4w;
+package BAEK_2609_최대공약수와최소공배수_5w;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		boolean[] prime = new boolean[10001];
 		//소수 = false 소수 아니면 true
 		prime[0] = true;
-		prime[1] = false;
+		prime[1] = true;
 		
 		for(int i = 2; i < 10000; i++) {
 			if(!prime[i]) {
@@ -28,34 +27,33 @@ public class Main {
 			}
 		}
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc= new Scanner(System.in);
 		
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
-		int divNum = 1;
-		int mulNum = 0;
-		boolean run = true;
-		if(num1 == num2) {
-			divNum = num1;
-			mulNum = num1;
-		}else {
-			while(run) {
-				for(int primeNum : primeList) {
-					if(num1 % primeNum == 0 && num2 % primeNum == 0) {
-						divNum *= primeNum;
-						
-					}else {
-						run = false;
-					}
-					
+		int min = 1;
+		int max = 0;
+		int N = num1;
+		int M = num2;
+		while(true) {
+			boolean check = false;
+			for(int primeNum : primeList) {
+				if(N % primeNum == 0 && M % primeNum == 0 ) {
+					min = min * primeNum;
+					N = N/primeNum;
+					M = M/primeNum;
+					check = true;
+					break;
 				}
 			}
-			mulNum = (num1*num2)/divNum;
+			if(!check) {
+				max = num1*num2/min;
+				System.out.println(min);
+				System.out.println(max);
+				break;
+			}
 		}
 		
-		
-		
-		System.out.println(divNum);
-		System.out.println(mulNum);
 	}
+
 }
