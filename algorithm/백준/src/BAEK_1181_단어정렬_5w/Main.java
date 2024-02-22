3d2e6776b5e9c7ee3ad2c786f02bf278e1e6b1a5
hwlee627 <juicyson627@gmail.com>
2024-02-22 15:38:15 +0900

    단어정렬 완료

--- algorithm/백준/src/BAEK_1181_단어정렬_5w/Main.java
+++ algorithm/백준/src/BAEK_1181_단어정렬_5w/Main.java
@@ -1,63 +1,57 @@
 package BAEK_1181_단어정렬_5w;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-		String[] words = new String[N];
-		int length = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		List<String> strArr = new ArrayList<String>();
 		for (int i = 0; i < N; i++) {
-			words[i] = sc.next();
+			strArr.add(br.readLine());
 		}
-		// c a b ab a abc d a b
-		int cnt = 0;
-		for (int i = 0; i < words.length; i++) {
-			if (i != words.length - 1) {
-				if (words[i].length() == words[i + 1].length()) {
-					if (words[i].charAt(0) == words[i + 1].charAt(0)) {
-						for (int j = 1; j < words[i].length(); j++) {
-							if (words[i].charAt(j) > words[i + 1].charAt(j)) {
-								String tmp = words[i];
-								words[i] = words[i + 1];
-								words[i + 1] = tmp;
-								break;
-							}
-						}
-					} else {
-						if(words[i].charAt(0) > words[i+1].charAt(0)) {
-							String tmp = words[i];
-							words[i] = words[i+1];
-							words[i+1] = tmp;
-							
-						}
-					}
-					if (words[i].equals(words[i + 1])) {
-						words[i] = " ";
-					}
-				} else if (words[i].length() > words[i + 1].length()) {
-					String tmp = words[i];
-					words[i] = words[i + 1];
-					words[i + 1] = tmp;
-				}
-			}
-			if (cnt == words.length) {
-				if (!words[i].equals(" ")) {
-					System.out.println(words[i]);
-				}
-			}
-			if (i == words.length - 1) {
-				if (cnt != words.length) {
-					cnt++;
-					i = -1;
-				}
-			}
+		//중복 없애기 List -> Set -> newList
+		Set<String> set = new HashSet<String>(strArr);
+		List<String> newList = new ArrayList<String>(set);
+		
+		//Natural order 쓰면 가나다순으로 바뀌는데 그 상태가 유지가 됨
+		//뭔 메커니즘인지는 모르겠음
+//		newList.sort(Comparator.naturalOrder());
+//		newList.sort(new LengthCompare());
+		
+		
+		newList.sort(new LengthCompare().thenComparing(Comparator.naturalOrder()));
+		
+		for(String words : newList) {
+			sb.append(words).append('\n');
 		}
+		System.out.println(sb);
 	}
+	
 
 }
+class LengthCompare implements Comparator<String>{
+	@Override
+	public int compare(String str1, String str2) {
+		if(str1.length() > str2.length()) {
+			return 1;
+		}else if(str1.length() == str2.length()) {
+			return 0;
+		}else {
+			return -1;
+		}
+	}
+}
+
