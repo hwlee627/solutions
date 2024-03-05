7952e413c09910fa8653d6f7397c767cd558b4a2
hwlee627 <juicyson627@gmail.com>
2024-03-05 13:39:51 +0900

    카드2 완료

---
+++ algorithm/백준/src/BAEK_2164_카드2_7w/Main.java
@@ -0,0 +1,29 @@
+package BAEK_2164_카드2_7w;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		Queue<Integer> q = new LinkedList<Integer>();
+		for(int i = 1; i<=N; i++) {
+			q.add(i);
+		}
+		
+		while(q.size()>1) {
+			q.poll();
+			int tmp = q.poll();
+			q.offer(tmp);
+		}
+		System.out.println(q.poll());
+	}
+
+}
